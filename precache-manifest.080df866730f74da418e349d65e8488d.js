a61617ce70782fe04cb9aa73e8b6ad79c651d4e2
frescobar <frescobar87@gmail.com>
2020-06-22 17:40:22 -0500

    Updates

---
+++ precache-manifest.080df866730f74da418e349d65e8488d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ad270f0fde3cb95c76936a97c076e5",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "2b7ff4737c6931a1508b",
+    "url": "/youtube-clone/static/css/main.02011ada.chunk.css"
+  },
+  {
+    "revision": "7d193b664f51aa906657",
+    "url": "/youtube-clone/static/js/2.df03ecca.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtube-clone/static/js/2.df03ecca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7ff4737c6931a1508b",
+    "url": "/youtube-clone/static/js/main.e3351ff7.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  }
+]);
